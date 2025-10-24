6db6e651230fe5493968ed0b5e8765f4651fe239
Arjunaadlina <arjunaadlinamartha@gmail.com>
2025-10-24 22:39:19 +0700

    delete shadow navbar

--- frontend/components/Navbar/Navbar.jsx
+++ frontend/components/Navbar/Navbar.jsx
@@ -208,7 +208,7 @@ export default function Navbar() {
   };
 
   return (
-    <nav className="bg-white shadow-md">
+    <nav className="bg-white">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         <Link href="/" className="flex items-center space-x-3">
           <Image 
